71a4b889eec77543ef81b3da77a5cd51c3669e6b
qicaisheng <qicaisheng@qq.com>
2019-06-23 11:47:53 +0800

    add hystrix

--- src/main/java/com/dmall/orderservice/OrderServiceApplication.java
+++ src/main/java/com/dmall/orderservice/OrderServiceApplication.java
@@ -2,10 +2,14 @@ package com.dmall.orderservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
+import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication
 @EnableSwagger2
+@EnableCircuitBreaker
+@EnableHystrixDashboard
 public class OrderServiceApplication {
 
     public static void main(String[] args) {
--- src/main/java/com/dmall/orderservice/adapter/inventory/InventoryClientImpl.java
+++ src/main/java/com/dmall/orderservice/adapter/inventory/InventoryClientImpl.java
@@ -1,5 +1,6 @@
 package com.dmall.orderservice.adapter.inventory;
 
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 import org.springframework.web.client.RestTemplate;
@@ -12,11 +13,17 @@ public class InventoryClientImpl implements InventoryClient {
 
     private RestTemplate restTemplate = new RestTemplate();
 
+    @HystrixCommand(fallbackMethod = "reliable")
     public String lock(Lock lock) {
         return restTemplate.postForEntity(inventoryServiceUrl + "/inventories/lock", lock, String.class).getBody();
     }
 
     public void unlock(String lockId) {
         restTemplate.put(inventoryServiceUrl + "/inventories/lock/" + lockId, null);
+    }
+
+
+    public String reliable(Lock lock) {
+        return "RELIABLE";
     }
 }
